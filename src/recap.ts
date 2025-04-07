5aa270aaf4b9c4852f4af4e2b038057e4810f481
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-04-06 21:12:51 -0600

    recap

---
+++ src/recap.ts
@@ -0,0 +1,18 @@
+//const suma = (a: number, b: number) => {
+//  return a + b;
+//};
+//suma(1, 2);
+//
+//class Persona {
+//  constructor(
+//    private age: number,
+//    private name: string,
+//  ) {}
+//
+//  getSummary() {
+//    return `my name es ${this.name}, ${this.age}`;
+//  }
+//}
+//
+//const Julieta = new Persona(21, 'Julieta');
+//Julieta.getSummary();
