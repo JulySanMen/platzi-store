fae3269eb7df4186d45f5c8c1387ce1ec02cb569
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-04-12 22:34:05 -0600

    parametros-query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -22,6 +22,16 @@ export class AppController {
   getProduct(@Param('productId') productId: string) {
     return `product ${productId}`;
   }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+  }
+
   @Get('categories/:id/products/:productId')
   getCategory(@Param('productId') productId: string, @Param('id') id: string) {
     return `product ${productId} and ${id}`;
