ddda33d53aa11f7ab9fbf8b31e24ea6a4ba3c1f6
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-04-15 14:58:03 -0600

    Put

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -31,5 +40,17 @@ export class ProductsController {
       mesage: 'accion de crear',
       payload,
     };
+  }
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return id;
   }
 }
