822a071ee20bf6fde7b4c9efc8c13f90426f5401
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-04-14 21:31:06 -0600

    Controller-brands

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { CategoriesController } from './controllers/categories.controller';
 import { OrdersController } from './controllers/orders.controller';
 import { UsersController } from './controllers/users.controller';
 import { CustomersController } from './controllers/customers.controller';
+import { BrandsController } from './controllers/brands.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController, OrdersController, UsersController, CustomersController],
+  controllers: [AppController, ProductsController, CategoriesController, OrdersController, UsersController, CustomersController, BrandsController],
   providers: [AppService],
 })
 export class AppModule {}
