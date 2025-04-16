bb83f45700dca8518f939cc733a5322c540f7321
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-04-15 18:36:15 -0600

    Put-Categories

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Param, Get } from '@nestjs/common';
+import { Controller, Param, Get, Put } from '@nestjs/common';
 
 @Controller('categories')
 export class CategoriesController {
   @Get(':id/products/:productId')
   getCategory(@Param('productId') productId: string, @Param('id') id: string) {
     return `product ${productId} and ${id}`;
+  }
+  @Put('id')
+  updateCategory(@Param('id') id: string) {
+    return { message: `update ${id}` };
   }
 }
