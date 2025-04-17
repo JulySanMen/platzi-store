b8ce993c62b33028c552c8814ad6523edbd26d43
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-04-16 20:01:09 -0600

    servisio-productos-memoria

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -3,6 +3,7 @@ import {} from './../entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private counterId = 1;
   private products = [
     {
       id: 1,
@@ -13,4 +14,19 @@ export class ProductsService {
       image: '',
     },
   ];
+  findAll() {
+    return this.products;
+  }
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+  create(payload: any) {
+    this.counterId = this.counterId + 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
 }
